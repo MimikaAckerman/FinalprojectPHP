effc0d2e3fa2a024a488d9948b9fac3ff6516d8e
Juan Carlos M Giner <jcmginer@gmail.com>
2022-10-10 14:30:36 +0200

    Added session in Router.php

--- core/Router.php
+++ core/Router.php
@@ -8,10 +8,19 @@ class Router
             $controllerName = $_GET['controller'] . "Controller";
             $controllerPath = CONTROLLERS . $controllerName . ".php";
             $fileExists = file_exists($controllerPath);
-            if ($fileExists) {
+            session_start();
+
+            if ($fileExists && isset($_SESSION['user'])) {
+
                 require_once $controllerPath;
                 $controller = new $controllerName;
-            } else {
+            } else if ($fileExists && !isset($_SESSION['user'])) {
+
+                $errorMsg = "You do not have any session started.";
+                require_once VIEWS . "error/error.php";
+            }
+            
+            else {
                 $errorMsg = "The page you are trying to access does not exist.";
                 require_once VIEWS . "error/error.php";
             }
