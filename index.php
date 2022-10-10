3d865d967f62aa8c841de6a6b17987a6436738df
Juan Carlos M Giner <jcmginer@gmail.com>
2022-10-10 11:04:27 +0200

    config.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once "config/constants.php";
+/* require_once "config/constants.php";
 require_once "config/db.php";
 require_once "core/classes/Database.php";
 require_once "core/classes/Model.php";
@@ -9,4 +9,8 @@ require_once "core/classes/Controller.php";
 
 require_once "core/Router.php";
 
-//$router = new Router();
+$router = new Router(); */
+
+require_once 'view/template/header.php';
+require_once 'view/'.$controller->view.'.php';
+require_once 'view/template/footer.php';
