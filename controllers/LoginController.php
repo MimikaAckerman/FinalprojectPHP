2edf4374e842072d11fdf08010919e8a576a1dbb
David Moreno <cesardaviddev@gmail.com>
2022-10-10 17:42:51 +0200

    new changes

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -1,6 +1,20 @@
 <?php
 
+require_once '../models/LoginModel.php';
+
 //Recive las peticiones y decidira a que metodo llamar del LoginModel.php
 
-$_POST['email'];
-$_POST['pass'];
+$email = $_POST['email'];
+
+$passw = $_POST['pass'];
+
+if($email == "cesardavidmor@gmail.com" && $passw == 1234){
+$login = new LoginModel();
+
+$login->getConection();
+$login -> getNotes();
+echo $login['id'];
+} else {
+  header("Location: ../views/error/error.php");
+}
+
