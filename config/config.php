33cf53a0085bd70e0d4384ca4cbba680e7680800
Jackson Zhou <jianchenfutbol@gmail.com>
2022-10-10 15:04:09 +0200

    loginController

--- config/config.php
+++ config/config.php
@@ -1,6 +1,10 @@
 <?php
+
 define('HOST', 'localhost');
 define('DB', 'notes');
 define('USER', 'root');
 define('PASSWORD', '');
 define('CHARSET', 'utf8mb4');
+
+
+
--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -1,3 +1,29 @@
 <?php
-
+require_once MODELS . 'models/LoginModel.php';
 //Recive las peticiones y decidira a que metodo llamar del LoginModel.php
+
+$action = "";
+
+if (isset($_REQUEST["action"])) {
+    $action = $_REQUEST["action"];
+}
+
+if (function_exists($action)) {
+    call_user_func($action, $_REQUEST);
+} else {
+    error("Invalid user action");
+}
+
+//CONTROLLER FUNCTIONS
+
+function getAllNotes()
+{
+    $notes = get();
+    if (isset($notes)){
+        require_once VIEWS . "";
+    } else {
+        error("There is a database error, try again.");
+    }
+}
+
+?>
