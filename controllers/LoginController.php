72a765c4fc0953fb4fdc1a3e097b99645b846ae3
Jackson Zhou <jianchenfutbol@gmail.com>
2022-10-11 12:46:20 +0200

    changes

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -1,19 +1,12 @@
 <?php
 
-function error($errorMsg)
-{
-    require_once VIEWS . "/error/error.php";
-}
-require_once '../models/LoginModel.php';
-
-
 //Recive las peticiones y decidira a que metodo llamar del LoginModel.php
 
 $email = $_POST['email'];
 
 $passw = $_POST['pass'];
 
-if($email == "cesardavidmor@gmail.com" && $passw == 1234){
+if($email == "admin" && $passw == 1234){
 
   header("Location: ../index.php?controller=User&action=getAllUsers");
 
--- index.php
+++ index.php
@@ -1,27 +1,4 @@
 <?php
-require_once 'config/db.php';
-/* require_once 'config/baseConstants.php'; */
-require_once 'models/db.php';
-
-if (!isset($_GET['controller'])) $_GET['controller'] = constant('DEFAULT_CONTROLLER');
-if (!isset($_GET['action'])) $_GET['action'] = constant('DEFAULT_ACTION');
-
-$controller_path = '/controllers/' . $_GET['controller'] . '.php';
-
-if (!file_exists($controller_path)) $controller_path = '/controllers/' . constant('DEFAULT_CONTROLLER') . '.php';
-
-require_once $controller_path;
-$controllerName = $_GET["controller"] . 'Controller';
-$controller = new $controllerName();
-
-/* Check if method is defined */
-$dataToView["data"] = array();
-if (method_exists($controller, $_GET["action"])) $dataToView["data"] = $controller->{$_GET["action"]}();
-
-
-
-
-
 
 require_once "config/constants.php";
 require_once "config/db.php";
@@ -34,12 +11,3 @@ require_once "core/Router.php";
 require_once "views/login/login.php";
 $router = new Router();
 
-/* require_once 'view/template/header.php';
-require_once 'view/'.$controller->view.'.php';
-<<<<<<< HEAD
-<<<<<<< HEAD
-require_once 'view/template/footer.php';
-
-=======
-require_once 'view/template/footer.php'; */
-require_once 'view/template/footer.php';
