e6dbce70ae2d407fb602d8bd4d4b5949706b0f63
Juan Carlos M Giner <jcmginer@gmail.com>
2022-10-12 17:08:26 +0200

    login modified

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -4,18 +4,6 @@
 
 //Recive las peticiones y decidira a que metodo llamar del LoginModel.php
 
-$email = $_POST['email'];
 
-$passw = $_POST['pass'];
-
-foreach ($this->data as $index => $user) {
-  if($email == $user["email"] && $passw == $user["password"]){
-
-    header("Location: ../index.php?controller=User&action=getAllUsers");
-
-  } else {
-    header("Location: ../error/error.php");
-  }
-}
 
 
