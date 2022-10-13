091cc6674b29dc191f99836492dfa0dbc1a57b1b
Juan Carlos M Giner <jcmginer@gmail.com>
2022-10-13 21:57:01 +0200

    Modified Login Controller

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -1,3 +1,29 @@
 <?php
 
-//Recive las peticiones y decidira a que metodo llamar del LoginModel.php
+class LoginController
+{
+
+    use Controller;
+
+    function login($request)
+    {
+        $log = null;
+
+        if (isset($request)) {
+
+            $log = $this->model->login($request);
+            if (isset($_SESSION['user'])) {
+                header("Location: index.php?controller=user&action=getAllUsers");
+            } else {
+                header("Location: index.php");
+            }
+        }
+    }
+
+    function logout()
+    {
+
+        session_destroy();
+        header("Location: index.php");
+    }
+}
