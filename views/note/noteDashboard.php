fb8f4f080df669f5fab32e61d402f4f6e4a3426d
Mimika Ackerman <emily.herreragomez@gmail.com>
2022-10-14 16:07:31 +0200

    modal favNotes listo

--- views/note/noteDashboard.php
+++ views/note/noteDashboard.php
@@ -10,12 +10,16 @@
 
 <!-- CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+
 <link rel="stylesheet" href="assets/css/noteDashboard.css" />
 
+
 <!-- JavaScript Bundle with Popper -->
-<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script> -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-<script src="/assets/js/utils.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+
+
+
 
 
 </head>
@@ -76,7 +80,7 @@
 
 
 <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+<button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
   Favorite notes
 </button>
 
@@ -90,12 +94,10 @@
       </div>
       <div class="modal-body">
 
-
-        ...
         <div class="theme_toogle">
                 <button class="theme_toogle_btn"></button>
             </div>
-            <span class="heading">My Day</span>
+            <span class="heading">Favorite Notes</span>
 
 
 
@@ -143,7 +145,7 @@
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Understood</button>
+    <!--     <button type="button" class="btn btn-primary">Understood</button> -->
       </div>
     </div>
   </div>
