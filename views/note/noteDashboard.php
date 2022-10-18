604da0a1bb42fa2575c77682d23c5fc19a40100c
Juan Carlos M Giner <jcmginer@gmail.com>
2022-10-18 11:25:48 +0200

    Style

--- views/note/noteDashboard.php
+++ views/note/noteDashboard.php
@@ -7,33 +7,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>TODO-list</title>
 
-    <link rel="stylesheet" href="assets/css/noteDashboard.css" />
+
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="assets/css/noteDashboard.css" />
 
     <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-
 
 </head>
 
 <body>
     <div class="container task_list_empty">
         <header>
-            <!-- <div class="theme_toogle">
-                <button class="theme_toogle_btn"></button>
-            </div> -->
             <span class="heading">My Day</span>
+        </header>
 
-
-
-            <!--ESTO ES EL CODIGO PHP-->
+        <main>
             <table class="table">
 
                 <thead>
                     <tr>
-                        <!--    <th class="tg-0pky">ID</th> -->
                         <th class="tg-0pky">Title</th>
                         <th class="tg-0lax">Content</th>
                         <th class="tg-0lax">Initial date</th>
@@ -44,7 +38,6 @@
                     <?php
                     foreach ($this->data as $index => $note) {
                         echo "<tr>";
-                        /*     echo "<td class='tg-0lax'>" . $note["id"] . "</td>"; */
                         echo "<td class='tg-0lax'>" . $note["title"] . "</td>";
                         echo "<td class='tg-0lax'>" . $note["content"] . "</td>";
                         echo "<td class='tg-0lax'>" . $note["initial_date"] . "</td>";
@@ -53,7 +46,7 @@
 
                         <a class='btn btn-secondary' href='?controller=Note&action=getNote&id=" . $note["id"] . "'>Edit</a>
                         <a class='btn btn-danger' href='?controller=Note&action=deleteNote&id=" . $note["id"] . "'>Delete</a>
-                        <a class='btn btn-success' href='?controller=Note&action=addFavNote&id=" . $note["id"] . "'>Add Fav.</a>
+                        <a class='btn btn-outline-success' href='?controller=Note&action=addFavNote&id=" . $note["id"] . "'>Add Fav.</a>
 
                         </td>";
 
@@ -64,22 +57,18 @@
             </table>
 
 
-        </header>
+
         <section class="tasks">
             <ul id="tasksList"></ul>
         </section>
+        </main>
         <footer>
-
 
             <a id="home" class="btn btn-light" href="?controller=Note&action=createNote">Create</a>
             <a id="home" class="btn btn-light" href="./">Back</a>
 
-
-            <!-- Button trigger modal -->
-            <a type="button" href="?controller=favNote&action=getAllFavNotes" class="btn btn-primary" style="margin-left: 137vh;">Favorite notes</a>
+            <a type="button" href="?controller=favNote&action=getAllFavNotes" class="btn btn-primary btn-fav">Favorite notes</a>
             <a id="home" class="btn btn-danger" href="?controller=Login&action=logout">Log out</a>
-
-
 
         </footer>
     </div>
